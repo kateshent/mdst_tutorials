5fbb5149a1e59b90930ea477f597d44f6f293285
kateshent <kshenton@umich.edu>
2020-10-06 23:11:51 -0400

    python exercises Tutorial 1

--- Tutorial1/python_exercises.py
+++ Tutorial1/python_exercises.py
@@ -6,12 +6,17 @@ def is_odd(x):
     """
     returns True if x is odd and False otherwise
     """
+    if x % 2 != 0:
+        return True
+    else:
+        return False
 
 
 def is_palindrome(word):
     """
     returns whether `word` is spelled the same forwards and backwards
     """
+    return word == word[::-1]
 
 
 def only_odds(numlist):
@@ -20,6 +25,12 @@ def only_odds(numlist):
 
     ex: only_odds([1, 2, 3, 4, 5, 6]) -> [1, 3, 5]
     """
+    oddlist = []
+    for i in numlist:
+        if i % 2 != 0:
+            oddlist.append(i)
+    return oddlist
+
 
 
 def count_words(text):
@@ -35,3 +46,9 @@ def count_words(text):
         {'how': 1, 'much': 1, 'wood': 1, 'would': 1, 'a': 2, 'woodchuck': 2,
         'chuck': 2, 'if': 1, 'could': 1, 'wood?': 1}
     """
+    wordCount = {}
+    for w in text.split():
+        if w in wordCount.keys():
+            wordCount[w] = wordCount[w] + 1
+        else:
+            wordCount[w] = 1
